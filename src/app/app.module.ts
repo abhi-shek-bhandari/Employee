c5dcf6883d7a02d96473c2e0003cbc20d4881e8b
abhi-shek-bhandari <abhishekbhandari06@outlook.com>
2023-06-23 01:33:14 +0530

    angular part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './MyComponents/table/table.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  { path: '', component: TableComponent },
+  { path: 'app-add-employee', component: AddEmployeeComponent,}
+  // Add more routes as needed
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    AddEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
