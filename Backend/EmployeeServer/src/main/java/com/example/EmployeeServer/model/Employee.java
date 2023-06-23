4b84f5f8ec21c3f1bb263e5b45b1495c6d72a98f
abhi-shek-bhandari <abhishekbhandari06@outlook.com>
2023-06-23 16:18:28 +0530

    Backend Updated

--- Backend/EmployeeServer/src/main/java/com/example/EmployeeServer/model/Employee.java
+++ Backend/EmployeeServer/src/main/java/com/example/EmployeeServer/model/Employee.java
@@ -1,6 +1,7 @@
 package com.example.EmployeeServer.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Entity
@@ -13,12 +14,28 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer employeeId;
 
+    @NotNull(message = "First name cannot be null")
+    @NotBlank(message = "First name cannot be blank")
+    @NotEmpty(message = "First name cannot be empty")
     private String firstName;
+
+    @NotNull(message = "Last name cannot be null")
+    @NotBlank(message = "Last name cannot be blank")
+    @NotEmpty(message = "Last name cannot be empty")
     private String lastName;
 
     @Column(unique = true)
+    @Email
+    @NotNull(message = "Email cannot be null")
+    @NotBlank(message = "Email cannot be blank")
+    @NotEmpty(message = "Email cannot be empty")
     private String email;
 
+    @NotNull(message = "Phone cannot be null")
+    @NotBlank(message = "Phone cannot be blank")
+    @NotEmpty(message = "Phone cannot be empty")
+    @Size(min=10,max=10,message="Mobile should be 10 characters.")
+    @Pattern(regexp = "^[6-9][0-9]{9}",message="Mobile should contains only numbers")
     @Column(unique = true)
     private String mobile;
 
